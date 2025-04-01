ec9e6163fa9f1d581c27be04595df1bf4749b486
weilycoder <weily2009@outlook.com>
2025-04-01 18:07:32 +0800

    Add a module to modify file content by adding backticks to math expressions

---
+++ trans.py
@@ -0,0 +1,27 @@
+"""
+This module modifies the content of a file by adding backticks to the math expressions.
+"""
+
+import re
+import sys
+
+
+def modify_file(file_path: str) -> None:
+    """
+    Modify the file content by adding backticks to the math expressions.
+
+    :param file_path: The path to the file to be modified.
+
+    :return: None
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    modified_content = re.sub(r"(?<!`)(\$(.+?)\$)", r"`\1`", content)
+
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(modified_content)
+
+
+if __name__ == "__main__":
+    modify_file(sys.argv[1])
