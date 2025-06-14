79642e8ea00eaa4c3d32984e109a7b6314016f6e
weilycoder <weily2009@outlook.com>
2025-06-14 19:25:33 +0800

    Update build time tracking to use ISO format and update subproject reference

--- scripts/update_build_time.js
+++ scripts/update_build_time.js
@@ -1,4 +1,5 @@
 hexo.extend.filter.register('before_generate', function () {
-  const moment = require('moment');
-  hexo.config.build_time = moment().format('YYYY-MM-DD HH:mm:ss');
+  const now = new Date();
+  const buildTime = now.toISOString(); // 获取 UTC 时间并存储为 ISO 格式
+  hexo.config.build_time = buildTime;
 });
