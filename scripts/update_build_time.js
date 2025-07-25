be46015d8d3ad1de081991c968fb53307816544e
weilycoder <weily2009@outlook.com>
2025-07-25 15:31:47 +0800

    feat: enable build time filtering and update subproject reference

--- scripts/update_build_time.js
+++
@@ -1,5 +0,0 @@
-hexo.extend.filter.register('before_generate', function () {
-  const now = new Date();
-  const buildTime = now.toISOString(); // 获取 UTC 时间并存储为 ISO 格式
-  hexo.config.build_time = buildTime;
-});
